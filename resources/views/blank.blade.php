df118e50d577a54070516066f5875179c62b361f
Diego Ribera <47877090+DRiberaC@users.noreply.github.com>
2023-11-28 14:57:51 -0400

    Update blank.blade.php

--- resources/views/blank.blade.php
+++ resources/views/blank.blade.php
@@ -8,16 +8,17 @@
     <title>Document</title>
 </head>
 
-<body class="relative bg-white-50 overflow-hidden max-h-screen">
+<body class="flex h-screen bg-gray-100">
 
-    <aside class="fixed inset-y-0 left-0 bg-white shadow-md max-h-screen w-60">
-        <div class="flex flex-col justify-between h-full">
-            <div class="flex-grow">
-                <div class="px-4 py-6 text-center border-b">
-                    <h1 class="text-xl font-bold leading-none"><span class="text-indigo-600">Fidelización</span> ROES
-                    </h1>
-                </div>
-                <div class="p-4">
+    <aside class="bg-white w-60 border-r overflow-y-auto h-screen flex flex-col">
+        <div>
+            <!-- Contenido del menú -->
+            <div class="px-4 py-6 text-center border-b">
+                <h1 class="text-xl font-bold leading-none"><span class="text-indigo-600">Fidelización</span> ROES</h1>
+            </div>
+            <div class="p-4">
+                <ul class="space-y-1">
+                    <!-- Opciones del menú -->
                     <ul class="space-y-1">
                         <li>
                             <a href="{{ route('cliente.index') }}"
@@ -66,26 +67,29 @@
                         </li>
 
                     </ul>
-                </div>
-            </div>
-            <div class="p-4">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit"
-                        class="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-                            class="" viewBox="0 0 16 16">
-                            <path
-                                d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                        </svg>
-                    </button> <span class="font-bold text-sm ml-2">Logout</span>
-                </form>
-
+                </ul>
             </div>
         </div>
+
+        <div class="mt-auto p-4">
+            <!-- Formulario de logout -->
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit"
+                    class="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
+                        class="" viewBox="0 0 16 16">
+                        <path
+                            d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                    </svg>
+                </button>
+                <span class="font-bold text-sm ml-2">Logout</span>
+            </form>
+        </div>
+
     </aside>
 
-    <main class="max-h-screen overflow-auto">
+    <main class="bg-white flex-1 p-4 overflow-y-auto">
         @yield('content')
     </main>
 </body>
