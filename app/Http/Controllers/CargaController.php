3fe871fc5e1dea8aa2dd2d75fce1d865dc1c84d6
Diego Ribera <47877090+DRiberaC@users.noreply.github.com>
2023-11-29 22:41:52 -0400

    Update CargaController.php

--- app/Http/Controllers/CargaController.php
+++ app/Http/Controllers/CargaController.php
@@ -26,45 +26,44 @@ class CargaController extends Controller
         $url = 'http://192.168.10.12:30004/reporteroes/2023-11-23?format=json';
         $token = env('TOKEN_XPSOLUTIONS');
 
-        try {
-            $response = $client->request('GET', $url, [
-                'headers' => [
-                    'auth-xpsolutions' => $token
-                ]
-            ]);
+        // try {
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'auth-xpsolutions' => $token
+            ]
+        ]);
 
-            $statusCode = $response->getStatusCode();
+        $statusCode = $response->getStatusCode();
 
-            if ($statusCode === 200) {
-                $content = $response->getBody()->getContents();
-                // Aquí puedes manejar la respuesta de la URL, que está en $content
-                // Por ejemplo, decodificar el JSON si es necesario
-                $data = json_decode($content, true);
-                // ... haz algo con los datos obtenidos
-                // dd($data);
+        if ($statusCode === 200) {
+            $content = $response->getBody()->getContents();
+            // Aquí puedes manejar la respuesta de la URL, que está en $content
+            // Por ejemplo, decodificar el JSON si es necesario
+            $data = json_decode($content, true);
+            // ... haz algo con los datos obtenidos
+            // dd($data);
 
-                foreach ($data as $item) {
-                    $carga = new Carga();
-                    $carga->id_referencia = $item['id'];
-                    $carga->observacion = $item['observacion'];
-                    $carga->total = $item['total'];
-                    $carga->nro_factura = $item['nro_factura'];
-                    $carga->fecha_venta = $item['fecha_venta'];
-                    $carga->razon_social = $item['razon_social'];
-                    $carga->nit = $item['nit'];
-                    $carga->cantidad = $item['cantidad'];
-                    $carga->precio = $item['precio'];
-                    $carga->user_id = null;
-                    $carga->save();
-                }
-
-                return redirect()->route('carga.index');
-            } else {
-                // Manejar otros códigos de estado si es necesario
+            foreach ($data as $item) {
+                $carga = new Carga();
+                $carga->id_referencia = $item['id'];
+                $carga->observacion = $item['observacion'];
+                $carga->total = $item['total'];
+                $carga->nro_factura = $item['nro_factura'];
+                $carga->fecha_venta = $item['fecha_venta'];
+                $carga->razon_social = $item['razon_social'];
+                $carga->nit = $item['nit'];
+                $carga->cantidad = $item['cantidad'];
+                $carga->precio = $item['precio'];
+                $carga->user_id = null;
+                $carga->save();
             }
-        } catch (\Exception $e) {
-            // Manejar errores de conexión u otros problemas
-            dd($e->getMessage()); // Esto es solo para depurar, puedes manejar el error como desees
+
+            return redirect()->route('carga.index');
+        } else {
+            // Manejar otros códigos de estado si es necesario
         }
+        // } catch (\Exception $e) {
+        //     dd($e->getMessage()); // Esto es solo para depurar, puedes manejar el error como desees
+        // }
     }
 }
