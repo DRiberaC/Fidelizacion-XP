7e34eb02b682755b5e66d6ea3441cb891ad01c43
Diego Ribera <47877090+DRiberaC@users.noreply.github.com>
2023-12-19 14:25:31 -0400

    Update index.blade.php

--- resources/views/carga/index.blade.php
+++ resources/views/carga/index.blade.php
@@ -42,7 +42,7 @@
                             </button>
                         </a>
                     </div>
-                    <div class="flex justify-center">
+                    {{-- <div class="flex justify-center">
                         <a href="{{ route('carga.obtenerfecha', [$fecha_act]) }}">
                             <button
                                 class="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
@@ -50,7 +50,7 @@
                                 Obtener Cargas de Fecha {{ $fecha_act }}
                             </button>
                         </a>
-                    </div>
+                    </div> --}}
                     <div class="flex justify-center">
                         <a href="{{ route('carga.index', [$fecha_sig]) }}">
                             <button
